4657f1105fcdb39a1ab0a7980d580f5ee6654d7a
kishorr4 <rajkishore.singh35@gmil.com>
2025-07-11 23:02:04 +0530

    bhai ka jalwa

---
+++ modules/azurerm_resource_group/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+#If you have made changes and want to save and exit:
+
+#Press Esc to make sure you’re in command mode (not insert mode).
+
